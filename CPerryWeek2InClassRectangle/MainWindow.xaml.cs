b6b0c02b7acf5d4f86b91c222298e3b604f46a8a
chrisperryNE <perry.chris@northeastern.edu>
2024-04-03 23:10:16 +0000

    changes added for final project.  initial git changes were not committed.

--- CPerryWeek2InClassRectangle/MainWindow.xaml.cs
+++ CPerryWeek2InClassRectangle/MainWindow.xaml.cs
@@ -15,26 +15,134 @@ using System.Windows.Shapes;
 
 namespace CPerryWeek2InClassRectangle
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-        
         }
 
         private void heightSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            rectangle.Height = heightSlider.Value;
+            /// in my initial version, I was missing the code on line 28.  Onece I realized this omission, I was surprised that it had been working as well as it had.
+            mainRectangle.Height = heightSlider.Value;
+           
+
+            if (areaSlider != null)
+            {
+                double rectWidth = areaSlider.Value / heightSlider.Value;
+                if(rectWidth > 780)
+                {
+                    mainRectangle.Width = 780;
+                }
+                else
+                {
+                    rectWidth = mainRectangle.Width;
+                }
+            }
+
+            mainEllipse.Height = heightSlider.Value;
+            if (mainEllipse.Height > 440)
+            {
+                mainEllipse.Height = 440;
+
+            }
+            
+            /// check to see if area slider exists.  If it is not null, you can proceed.
+            ///if (areaSlider != null)
+            ///{
+                ///double ellipseWidth = heightSlider.Value;
+                ///if (ellipseWidth > 780)
+                ///{
+                   /// mainEllipse.Width = 780;
+               /// }
+               /// else
+               /// {
+                ///    ellipseWidth = mainEllipse.Width;
+                ///}
+            ///}
         }
 
-        private void widthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void areaSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            rectangle.Width = widthSlider.Value;
+
+            double rectanglewidth = areaSlider.Value / heightSlider.Value;
+            mainRectangle.Width = rectanglewidth;
+            ///At some point, I had the if/else statement below swapped, which didn't work and the width of the rectangle was able to go far beyond the window.
+            if (rectanglewidth > 760)
+            {
+                mainRectangle.Width = 760;
+            }
+            else
+            {
+                mainRectangle.Width = rectanglewidth;
+            }
+
+
+            double ellipseWidth = areaSlider.Value / heightSlider.Value;
+            mainEllipse.Width = ellipseWidth;
+
+            if (ellipseWidth > 760)
+            {
+                mainEllipse.Width = 760;
+            }
+            else
+            {
+                mainEllipse.Width = ellipseWidth;
+            }
         }
+        private void selectRectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            ///I created this to select the shape and add the color so that it was clear which shape was selected.
+            
+            if (mainRectangle.Visibility == Visibility.Hidden)
+            {
+                mainRectangle.Visibility = Visibility.Visible;
+                selectRectangle.Fill = new SolidColorBrush(Color.FromRgb(13, 182, 58));
+
+                mainEllipse.Visibility = Visibility.Hidden;
+                selectEllipse.Fill = new SolidColorBrush(Color.FromRgb(244, 244, 245));
+
+                
+            }
+            else
+            {
+                mainRectangle.Visibility = Visibility.Hidden;
+                selectRectangle.Fill = new SolidColorBrush(Color.FromRgb(244, 244, 245));
+
+                mainEllipse.Visibility = Visibility.Visible;
+                selectEllipse.Fill = new SolidColorBrush(Color.FromRgb(13, 182, 58));
+
+                
+            }
+        }
+
+        private void selectEllipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (mainEllipse.Visibility == Visibility.Hidden)
+            {
+                mainEllipse.Visibility = Visibility.Visible;
+                selectEllipse.Fill = new SolidColorBrush(Color.FromRgb(13, 182, 58));
+
+                mainRectangle.Visibility = Visibility.Hidden;
+                selectRectangle.Fill = new SolidColorBrush(Color.FromRgb(244, 244, 245));
+
+                
+            }
+            else
+            {
+                mainEllipse.Visibility = Visibility.Hidden;
+                selectEllipse.Fill = new SolidColorBrush(Color.FromRgb(244, 244, 245));
+
+                mainRectangle.Visibility = Visibility.Visible;
+                selectRectangle.Fill = new SolidColorBrush(Color.FromRgb(13, 182, 58));
+
+                
+            }
+        }
+
+        
     }
        
 }
